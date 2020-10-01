350eeaa93c5cd71108b3483d658eea63046b3233
odunayo12 <odun.rotimi@gmail.com>
2020-10-01 12:56:24 +0200

    Accept Current Changes

--- 03/3.02/js/main.js
+++ 03/3.02/js/main.js
@@ -1,5 +1,4 @@
 /*
-<<<<<<< HEAD
  *    main.js
  *    Mastering Data Visualization with D3.js
  *    3.2 - Linear scales
@@ -37,33 +36,3 @@ d3.json("data/buildings.json").then(function (data) {
       return "grey";
     });
 });
-=======
-*    main.js
-*    Mastering Data Visualization with D3.js
-*    3.2 - Linear scales
-*/
-
-const svg = d3.select("#chart-area").append("svg")
-  .attr("width", 400)
-  .attr("height", 400)
-
-d3.json("data/buildings.json").then(data => {
-  data.forEach(d => {
-    d.height = Number(d.height)
-  })
-
-  const y = d3.scaleLinear()
-    .domain([0, 828])
-    .range([0, 400])
-
-  const rects = svg.selectAll("rect")
-    .data(data)
-  
-  rects.enter().append("rect")
-    .attr("y", 0)
-    .attr("x", (d, i) => (i * 60))
-    .attr("width", 40)
-    .attr("height", d => y(d.height))
-    .attr("fill", "grey")
-})
->>>>>>> 418f888bab265507b5431c03399ff97a0c22e106
--- 03/3.07/js/main.js
+++ 03/3.07/js/main.js
@@ -4,7 +4,6 @@
  *    3.7 - D3 min, max, and extent
  */
 
-<<<<<<< HEAD
 var svg = d3
   .select("#chart-area")
   .append("svg")
@@ -54,34 +53,3 @@ d3.json("data/buildings.json").then(function (data) {
     })
     .attr("fill", "grey");
 });
-=======
-const svg = d3.select("#chart-area").append("svg")
-  .attr("width", 400)
-  .attr("height", 400)
-
-d3.json("data/buildings.json").then(data => {
-  data.forEach(d => {
-    d.height = Number(d.height)
-  })
-
-  const x = d3.scaleBand()
-    .domain(data.map(d => d.name))
-    .range([0, 400])
-    .paddingInner(0.3)
-    .paddingOuter(0.2)
-  
-  const y = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.height)])
-    .range([0, 400])
-
-  const rects = svg.selectAll("rect")
-    .data(data)
-  
-  rects.enter().append("rect")
-    .attr("y", 0)
-    .attr("x", (d) => x(d.name))
-    .attr("width", x.bandwidth)
-    .attr("height", d => y(d.height))
-    .attr("fill", "grey")
-})
->>>>>>> 418f888bab265507b5431c03399ff97a0c22e106
