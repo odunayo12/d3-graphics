706dff16f662c47d3b84f6f46d461f734ac4d1f9
odunayo12 <odun.rotimi@gmail.com>
2020-12-01 19:00:16 +0100

    accept changes

--- 05/5.02/js/main.js
+++ 05/5.02/js/main.js
@@ -1,149 +1,22 @@
 /*
-<<<<<<< HEAD
  *    main.js
  *    Mastering Data Visualization with D3.js
  *    5.2 - Looping with intervals
  */
 
-var svg = d3
+const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 };
+const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT;
+const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM;
+
+const svg = d3
   .select("#chart-area")
   .append("svg")
-  .attr("width", 600)
-  .attr("height", 600)
-  .append("g")
-  .attr("transform", "translate(50, 50)");
-
-var data = [
-  { child: "John", parent: "" },
-  { child: "Aaron", parent: "Kevin" },
-  { child: "Kevin", parent: "John" },
-  { child: "Ann", parent: "John" },
-  { child: "Mark", parent: "Ann" },
-  { child: "Sarah", parent: "Kevin" },
-  { child: "Isaac", parent: "Sarah" },
-  { child: "Hannah", parent: "Ann" },
-  { child: "Rose", parent: "Sarah" },
-];
-
-// takes id and parentId
-var dataStructure = d3
-  .stratify()
-  .id((d) => d.child)
-  .parentId((d) => d.parent)(data);
-
-var treeStructure = d3.tree().size([500, 300]); //perhaps (x,y) coordinates
-
-var information = treeStructure(dataStructure);
-
-console.log(information.descendants());
-console.log(information.links());
-
-var circles = svg
-  .append("g")
-  .selectAll("circle")
-  .data(information.descendants());
-
-circles
-  .enter()
-  .append("circle")
-  .attr("cx", (d) => d.x)
-  .attr("cy", (d) => d.y)
-  .attr("r", 5);
-
-// var margin = { left:80, right:20, top:50, bottom:100 };
-//
-// var width = 600 - margin.left - margin.right,
-//     height = 400 - margin.top - margin.bottom;
-//
-// var g = d3.select("#chart-area")
-//     .append("svg")
-//         .attr("width", width + margin.left + margin.right)
-//         .attr("height", height + margin.top + margin.bottom)
-//     .append("g")
-//         .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
-//
-// // X Label
-// g.append("text")
-//     .attr("y", height + 50)
-//     .attr("x", width / 2)
-//     .attr("font-size", "20px")
-//     .attr("text-anchor", "middle")
-//     .text("Month");
-//
-// // Y Label
-// g.append("text")
-//     .attr("y", -60)
-//     .attr("x", -(height / 2))
-//     .attr("font-size", "20px")
-//     .attr("text-anchor", "middle")
-//     .attr("transform", "rotate(-90)")
-//     .text("Revenue");
-//
-// d3.json("data/revenues.json").then(function(data){
-//     // console.log(data);
-//
-//     // Clean data
-//     data.forEach(function(d) {
-//         d.revenue = +d.revenue;
-//     });
-//
-//     // X Scale
-//     var x = d3.scaleBand()
-//         .domain(data.map(function(d){ return d.month }))
-//         .range([0, width])
-//         .padding(0.2);
-//
-//     // Y Scale
-//     var y = d3.scaleLinear()
-//         .domain([0, d3.max(data, function(d) { return d.revenue })])
-//         .range([height, 0]);
-//
-//     // X Axis
-//     var xAxisCall = d3.axisBottom(x);
-//     g.append("g")
-//         .attr("class", "x axis")
-//         .attr("transform", "translate(0," + height +")")
-//         .call(xAxisCall);
-//
-//     // Y Axis
-//     var yAxisCall = d3.axisLeft(y)
-//         .tickFormat(function(d){ return "$" + d; });
-//     g.append("g")
-//         .attr("class", "y axis")
-//         .call(yAxisCall);
-//
-//     // Bars
-//     var rects = g.selectAll("rect")
-//         .data(data)
-//
-//     rects.enter()
-//         .append("rect")
-//             .attr("y", function(d){ return y(d.revenue); })
-//             .attr("x", function(d){ return x(d.month) })
-//             .attr("height", function(d){ return height - y(d.revenue); })
-//             .attr("width", x.bandwidth)
-//             .attr("fill", "grey");
-//
-//     d3.interval(function(){
-//         console.log("Hello World");
-//     }, 1000);
-// });
-=======
-*    main.js
-*    Mastering Data Visualization with D3.js
-*    5.2 - Looping with intervals
-*/
-
-const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 }
-const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT
-const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM
-
-const svg = d3.select("#chart-area").append("svg")
   .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
-  .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+  .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM);
 
-const g = svg.append("g")
-  .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
+const g = svg
+  .append("g")
+  .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
 
 // X label
 g.append("text")
@@ -152,63 +25,64 @@ g.append("text")
   .attr("y", HEIGHT + 60)
   .attr("font-size", "20px")
   .attr("text-anchor", "middle")
-  .text("Month")
+  .text("Month");
 
 // Y label
 g.append("text")
   .attr("class", "y axis-label")
-  .attr("x", - (HEIGHT / 2))
+  .attr("x", -(HEIGHT / 2))
   .attr("y", -60)
   .attr("font-size", "20px")
   .attr("text-anchor", "middle")
   .attr("transform", "rotate(-90)")
-  .text("Revenue ($)")
+  .text("Revenue ($)");
 
-d3.csv("data/revenues.csv").then(data => {
-  data.forEach(d => {
-    d.revenue = Number(d.revenue)
-  })
+d3.csv("data/revenues.csv").then((data) => {
+  data.forEach((d) => {
+    d.revenue = Number(d.revenue);
+  });
 
-  const x = d3.scaleBand()
-    .domain(data.map(d => d.month))
+  const x = d3
+    .scaleBand()
+    .domain(data.map((d) => d.month))
     .range([0, WIDTH])
     .paddingInner(0.3)
-    .paddingOuter(0.2)
-  
-  const y = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.revenue)])
-    .range([HEIGHT, 0])
+    .paddingOuter(0.2);
 
-  const xAxisCall = d3.axisBottom(x)
+  const y = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.revenue)])
+    .range([HEIGHT, 0]);
+
+  const xAxisCall = d3.axisBottom(x);
   g.append("g")
     .attr("class", "x axis")
     .attr("transform", `translate(0, ${HEIGHT})`)
     .call(xAxisCall)
     .selectAll("text")
-      .attr("y", "10")
-      .attr("x", "-5")
-      .attr("text-anchor", "end")
-      .attr("transform", "rotate(-40)")
+    .attr("y", "10")
+    .attr("x", "-5")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-40)");
 
-  const yAxisCall = d3.axisLeft(y)
+  const yAxisCall = d3
+    .axisLeft(y)
     .ticks(3)
-    .tickFormat(d => d + "m")
-  g.append("g")
-    .attr("class", "y axis")
-    .call(yAxisCall)
+    .tickFormat((d) => d + "m");
+  g.append("g").attr("class", "y axis").call(yAxisCall);
 
-  const rects = g.selectAll("rect")
-    .data(data)
-  
-  rects.enter().append("rect")
-    .attr("y", d => y(d.revenue))
+  const rects = g.selectAll("rect").data(data);
+
+  rects
+    .enter()
+    .append("rect")
+    .attr("y", (d) => y(d.revenue))
     .attr("x", (d) => x(d.month))
     .attr("width", x.bandwidth)
-    .attr("height", d => HEIGHT - y(d.revenue))
-    .attr("fill", "grey")
+    .attr("height", (d) => HEIGHT - y(d.revenue))
+    .attr("fill", "grey");
 
   d3.interval(() => {
-    console.log("Hello World")
-  }, 1000)
-})
->>>>>>> 418f888bab265507b5431c03399ff97a0c22e106
+    console.log("Hello World");
+  }, 1000);
+});
